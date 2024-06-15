d29fd70bee7727500f62a158c5fcef9b39e51772
Sema Topcu <ssemaatopcu.@gmail.com>
2024-06-15 23:35:50 +0300

    adding a barrier and describing the barrier's four corners

--- GameCode.swift
+++ GameCode.swift
@@ -1,8 +1,24 @@
 import Foundation
 
 let circle = OvalShape(width: 150, height: 150)
+let barrierWidth = 300.0
+let barrierHeight = 25.0
+
+let barrierPoints = [
+Point(x: 0, y: 0),
+Point(x: 0, y: barrierHeight),
+Point(x: barrierWidth, y: barrierHeight),
+Point(x: barrierWidth, y: 0)
+]
+
+let barrier = PolygonShape(points: barrierPoints)
 
 func setup() {
     circle.position = Point(x: 150, y: 350)
     scene.add(circle)
+    circle.hasPhysics = true
+    
+    barrier.position = Point(x: 100, y: 200)
+    scene.add(barrier)
+    barrier.hasPhysics = true
 }
