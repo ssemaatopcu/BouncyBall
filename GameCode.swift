879b0c72450f5ada321e3f9a06392ba870406cbe
Sema Topcu <ssemaatopcu.@gmail.com>
2024-06-15 23:27:14 +0300

    adding first shape

--- GameCode.swift
+++ GameCode.swift
@@ -1,15 +1,8 @@
 import Foundation
 
-/*
-The setup() function is called once when the app launches. Without it, your app won't compile.
-Use it to set up and start your app.
-
-You can create as many other functions as you want, and declare variables and constants,
-at the top level of the file (outside any function). You can't write any other kind of code,
-for example if statements and for loops, at the top level; they have to be written inside
-of a function.
-*/
+let circle = OvalShape(width: 150, height: 150)
 
 func setup() {
-    
+    circle.position = Point(x: 150, y: 350)
+    scene.add(circle)
 }
